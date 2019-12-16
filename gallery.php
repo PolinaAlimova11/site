4d1877a95247e3be8e14697f05170b71b600723d
PolinaAlimova11 <56290572+PolinaAlimova11@users.noreply.github.com>
2019-12-16 18:11:21 +0300

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -6,14 +6,14 @@
     exit();
 		}
 	mysql_query('SET NAMES utf8');
-	$sql1 = mysqli_query($mysql, "SELECT * FROM `imag`");
-	//$sql2 = mysqli_query($mysql, "SELECT * FROM `comment`");
+	
 ?>
 <html>
 	<head>
 		<meta charset="uft-8">
 		<link rel="stylesheet" href="style.css">
 		<title>Галерея Sould</title>
+		<link rel="shortcut icon" href="glav.jpg" type="image/jpg">
 	</head>
 	<body>
 	<div id="zero">
@@ -24,39 +24,83 @@
 		
 		
 		<div id="three">
-			<a href="main.html"><div id="three-one"><p>главная</p></div></a>
+			<a href="main.php"><div id="three-one"><p>главная</p></div></a>
 			<a href="gallery.php"><div id="three-two"><p>галерея</p></div></a>
-			<a href="comm.html"><div id="three-three"><p>комментарии</p></div></a>
+			<a href="comm.php"><div id="three-three"><p>комментарии</p></div></a>
 			<a href="back.php"><div id="three-foor"><p>профиль</p></div></a>
 		</div>
 		
-		<div id="foor" style="height: auto; min-height: 1000px;">
-			
+		<div id="foor" style="height: auto; min-height: 500px;">
 			<?php
-				 while ($user1 = $sql1->fetch_assoc()) {
+				$quantity=4;
+				$limit=3;
+				$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+				if ($page<1) $page=1;
+				$result2 = mysqli_query($mysql,"SELECT * FROM `imag`");
+				$num = mysqli_num_rows($result2);
+				$pages = $num/$quantity;
+				$pages = ceil($pages);
+				$pages++; 
+				if ($page>$pages) $page = 1;
+				echo '<strong style="color: rgb(40,40,40);">Страница № ' . $page . 
+				'</strong><br /><br />'; 
+				if (!isset($list)) $list=0;
+				$list=($page-1)*$quantity;
+				$result = mysqli_query($mysql, "SELECT * FROM `imag`
+                      LIMIT $quantity OFFSET $list");
+				$num_result = mysqli_num_rows($result);
+				for ($i = 0; $i<$num_result; $i++) {
+						//маленький блок
+						$user1 = $result->fetch_assoc();
 						echo '<div style="height: auto;" class="cart"><a style="color: rgb(230,230,230);" href="#ok';
-						echo $user1['id'];
+						$user_id=$user1['id'];
+						//echo $user1['id'];
+						echo $user_id;
 						echo '">';
 						echo $user1['name'];
 						echo "<img src=\"data:image/jpg;base64,".base64_encode($user1['img_min'])."\" /><br>";
 						echo '</div></a>';
+						//конец маленького блока
+						//большой блок
 						echo '<div class="okno" id="ok';
-						echo $user1['id'];
-						echo '">
-						<a href="#close" title="Закрыть" class="close">X</a>
+						echo $user1['id'];//НАКАТИТЬ НОРМАЛЬНЫЙ СТИЛЬ
+						echo '">';
+						//РЕДАКТИРОВАТЬ
+						if (isset($_SESSION['name'])and ($_SESSION['status']=='adm' )) {
+						echo '<form action="red.php" class="like" method="post">
+						<input type="hidden" name="red" value="';
+						echo $user_id;
+						
+						echo'" />';
+						echo '<input type="hidden" name="nam" value="';
+							echo $page;
+							echo '"/>
+						<button style="background-color: rgb(150,150,150);;"class="btn btn-success" type="submit">Редактировать</button>
+						</form>';
+						}
+						echo '<a href="#close" title="Закрыть" class="close">X</a>
 						<h2>';
 						echo $user1['name'];
-						echo '</h2>';
+						echo '</h2><br>';
 				 
 						echo "<img style='width: 70%;' src=\"data:image/jpg;base64,".base64_encode($user1['img_max'])."\" /><br><br>";
 						//мне нравится кнопка
-						echo '<form action="like.php" class="like" method="post">
-						<input type="hidden" name="like_id" value="';
-						echo $user1['id'];
-						echo'" />
-						<button class="btn btn-success" type="submit">Мне нравится</button>
-						</form>';
+						if (isset($_SESSION['name'])and ($_SESSION['status']=='adm' or $_SESSION['status']=='user')) {
+							echo '<form action="like.php" class="like" method="post">
+							<input type="hidden" name="like_id" value="'.$user_id .'" />';
+							echo '<input type="hidden" name="nam" value="';
+							echo $page;
+							echo '"/>
+							</p><button class="btn btn-success" type="submit">Мне нравится ';
+							echo $user1['nrav'] .'</button>
+							</form>';
+						}
+						else {
+							echo 'Мне нравится ';
+							echo $user1['nrav'];
+						}
 						//конец кнопки
+						
 						echo "<p style=' text-align: center;'>";
 						echo $user1['data'];
 						echo ' ';
@@ -69,7 +113,7 @@
 						//начало блока комментариев
 						//
 						$sql2 = mysqli_query($mysql, "SELECT * FROM `comment`");
-						echo '<h3>Комментарии</h3>';
+						echo '<br><h3>Комментарии</h3><br>';
 						echo'<div style="display: block; overflow: auto; color: rgb(30,30,30); background-color: rgb(220,220,220); width: 70%; height: 200px; padding: 10px; margin-left: 15%;">';
 							while ($uscom = $sql2->fetch_assoc()) {
 									if ($uscom['name2']==$user1['id']) {
@@ -84,20 +128,34 @@
 								}					
 						echo'</div><br>';
 								//комментарий добавить
-						if (isset($_SESSION['name'])and $_SESSION['status']=='adm' or $_SESSION['status']=='user') {		
+						if (isset($_SESSION['name'])and ($_SESSION['status']=='adm' or $_SESSION['status']=='user')) {		
 							echo "<form action='com_obr.php' method='post'>
 							<p><b>Введите ваш комментарий:</b></p> <br>
 							<textarea style='height: 50px; width: 70%;' name='comment' class='form1'></textarea> <br>
 							<input type='hidden' name='img_id' value='";
 							echo $user1['id'];
 							echo "'/>
+							<input type='hidden' name='nam' value='";
+							echo $page;
+							echo "'/>
 							<button class='btn btn-success' type='submit'>Отправить</button>
 							</form>";
 						}
 						//конец 
 						echo "</div>";
+						//конец большого блока
 					
-					}
+					}			
+
+				//Тут адресация на другие страницы
+				echo '<br>';
+				echo 'Страницы: ';
+				for ($j = 1; $j<$pages; $j++) {
+						if ($j==($page)) {
+							echo '<a href="gallery.php?page=' . $j . '"><strong style="color: rgb(40,40,40);">' . $j . '</strong></a> &nbsp; ';
+						}
+						 else echo '<a href="gallery.php?page=' . $j . '">' . $j . '</a> &nbsp; ';
+				}
 			?>
 			<div class="okno" id="ok">
 				<a href="#close" title="Закрыть" class="close">X</a>
